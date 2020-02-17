59b421abcf0d8ae6cee494bd9efe7904354f7219
Michiel Goethals <goethalsmichiel@MacBook-Pro-van-Michiel-2.local>
2020-02-17 12:28:30 +0100

    final result for proof of concept

--- main.cpp
+++ main.cpp
@@ -8,7 +8,6 @@ BME280 TphI2c = BME280(D0, D1, addr);
 
 int main() {
     while(1) {
-    pc.printf("\n IC2");
     pc.printf("\n Humidity = %lf", TphI2c.getHumidity());
     pc.printf("\n Temperature = %lf", TphI2c.getTemperature());
     pc.printf("\n Presure = %lf", TphI2c.getPressure());
