0910fdb6470c648169cf2edae737dc0691411e0f
Michiel Goethals <goethalsmichiel@MacBook-Pro-van-Michiel-2.local>
2020-02-17 12:20:20 +0100

    added the mbed program and libraries

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include "mbed.h"
+#include "BME280.h"
+
+Serial pc(USBTX,USBRX); 
+char addr = 0x76 << 1;
+
+BME280 TphI2c = BME280(D0, D1, addr);
+
+int main() {
+    while(1) {
+    pc.printf("\n IC2");
+    pc.printf("\n Humidity = %lf", TphI2c.getHumidity());
+    pc.printf("\n Temperature = %lf", TphI2c.getTemperature());
+    pc.printf("\n Presure = %lf", TphI2c.getPressure());
+    wait(2);
+    }
+
+}
